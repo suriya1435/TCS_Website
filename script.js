dca9bcab819400bba7d7888add1836c3e7786926
suriya1435 <131662936+suriya1435@users.noreply.github.com>
2026-01-01 11:16:01 +0530

    aboutus.css

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 function sendMail(){
-	let parms = {
-	from_name : document.getElementById("name").value,
-	email : document.getElementById("email").value,
-	phone_number:document.getElementById("phone_number").value,
-	message:document.getElementById("message").value,
-	}
-	
-	emailjs.send("service_5t27vdv", "template_ll0of7k", parms)
-    .then(function(response) {
-        alert("Message Sent Successfully!");
-        document.querySelector("form").reset();
-    })
-    .catch(function(error) {
-        alert("Failed to send email. Check console.");
-        console.log(error);
-    });
+    let parms ={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone_number").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    }
+
+    emailjs.send("service_5t27vdv", "template_ll0of7k", parms).then(alert("Email Sent!!"))
+
 }
